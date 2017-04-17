04917a49a3a842065307ffcc60da3eed4562d0a4
Daniel Lawrence <64.74.6c@gmail.com>
2017-04-17 19:02:22 -0400

    Added 200..1000 sphere tests

--- perftest.sh
+++ perftest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "nSpheres\tTimeTaken" > performance.dat
-for s in 1 2 3 4 5 10 15 20 50 100
+for s in 1 2 3 4 5 10 15 20 50 100 200 300 400 500 1000
 do
   echo -ne "$s\t" >> performance.dat
   ./raytrace.out $s >> performance.dat
